8a225292fcee3e185eba2e079f44082c88352e40
DHAMODHARABALAJI R <DHAMODHARABALAJI.R@cba.com.au>
2025-03-20 08:40:20 +0530

    ..

--- order-manager/src/handlers/getOrder.js
+++ order-manager/src/handlers/getOrder.js
@@ -1,18 +1,18 @@
-const connectDB = require("../utils/db");
-const Order = require("../models/orderModel");
-const { getCache, setCache } = require("../utils/cache");
+const dbContext = require("../utils/dbContext");
+// const { getCache, setCache } = require("../utils/cache");
 
 exports.handler = async (event) => {
-  await connectDB();
 
-  const orderId = event.pathParameters.id;
-  let order = await getCache(`order:${orderId}`);
+  const id = event.pathParameters.id;
+  // let order = await getCache(`order:${orderId}`);
 
-  if (!order) {
-    order = await Order.findOne({ orderId });
-    if (!order) return { statusCode: 404, body: "Order not found" };
-    await setCache(`order:${orderId}`, order);
-  }
+  // if (!order) {
+  //   order = await Order.findOne({ orderId });
+  //   if (!order) return { statusCode: 404, body: "Order not found" };
+  //   await setCache(`order:${orderId}`, order);
+  // }
+
+  const order = dbContext.Orders().Get({ id });
 
   return {
     statusCode: 200,
